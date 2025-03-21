b67f6b8165c779eaa4edc6766edb5f883b6db14f
Zachary <zacharywerthman@gmail.com>
2025-03-21 15:44:41 -0500

    need to fix online_async_launch

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -4,7 +4,11 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import RegisterEventHandler, IncludeLaunchDescription, TimerAction
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -40,7 +44,6 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
 
-
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
@@ -53,11 +56,71 @@ def generate_launch_description():
         arguments=["joint_broad"],
     )
 
+    lidar_ramap = Node(
+        package='laser_filters',
+        executable='scan_to_scan_filter_chain',
+        name='laser_filter',
+        parameters=[PathJoinSubstitution([
+            FindPackageShare('my_bot'),
+            'config',
+            'lidar_params.yaml'
+        ])],
+
+        remappings=[
+            ('/scan','/filtered_scan')
+        ]
+    )
+
+    #launches the SLAM toolbox map
+    online_async = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','online_async_launch.py'
+                )])       
+    )
+    
+    #Defines the costmap launch file
+    nav2_bringup = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('nav2_bringup'),'launch','navigation_launch.py' 
+                )])
+    )
+
+    #Waits for 12 seconds until launching the costmap
+    delayed_nav2_bringup = TimerAction(period=15.0, actions=[nav2_bringup])
+
+
+    #Takes all sensor data and combines for odometry
+    robot_localization = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('robot_localization'), 'launch', 'ekf.launch.py'
+                )])
+    )
+
+    #create odom from lidar
+    lidar_odom = Node(
+        package = 'simple_2d_lidar_odometry',
+        executable = 'lidar_odometry_node'
+    )
+
+    #mpu6050
+    # imu = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory('ros2_mpu6050'), 'launch', 'ros2_mpu6050.launch.py'
+    #             )])
+    # )
+
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
         diff_drive_spawner,
-        joint_broad_spawner
+        joint_broad_spawner,
+        #lidar_ramap
+        online_async
+        #delayed_nav2_bringup,
+        #robot_localization,
+        #lidar_odom
+        #imu
+
     ])
